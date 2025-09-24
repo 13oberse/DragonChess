904f0ce8821f5bcd48b5b03b6905d5991fc92a43
13oberse <oberse@proton.me>
2025-09-24 11:20:13 -0400

    added known proxy

--- src/DragonChessUI/Program.cs
+++ src/DragonChessUI/Program.cs
@@ -3,49 +3,54 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace DragonChessUI;
 
 public static class Program
 {
-    public static async Task Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
-        {
-            Args = args,
+	public static async Task Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+		{
+			Args = args,
 #if !DEBUG
             ContentRootPath = "/app"
 #endif
-        });
+		});
 
-        // Add services to the container.
-        builder.Services.AddRazorPages();
-        builder.Services.AddServerSideBlazor();
-        builder.Services.AddSingleton<WeatherForecastService>();
-        builder.Services.AddPlayerManager();
+		builder.Services.Configure<ForwardedHeadersOptions>(options =>
+		{
+			options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+			options.KnownProxies.Clear();
+			options.KnownProxies.Add(IPAddress.Parse("172.16.6.1"));
+		});
 
-        var app = builder.Build();
-        app.UseForwardedHeaders(new ForwardedHeadersOptions
-        {
-            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-        });
+		// Add services to the container.
+		builder.Services.AddRazorPages();
+		builder.Services.AddServerSideBlazor();
+		builder.Services.AddSingleton<WeatherForecastService>();
+		builder.Services.AddPlayerManager();
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
+		var app = builder.Build();
+		app.UseForwardedHeaders();
 
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseRouting();
+		// Configure the HTTP request pipeline.
+		if (!app.Environment.IsDevelopment())
+		{
+			app.UseExceptionHandler("/Error");
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
+		}
 
-        app.MapBlazorHub();
-        app.MapFallbackToPage("/_Host");
+		app.UseHttpsRedirection();
+		app.UseStaticFiles();
+		app.UseRouting();
 
-        await app.RunAsync();
-    }
+		app.MapBlazorHub();
+		app.MapFallbackToPage("/_Host");
+
+		await app.RunAsync();
+	}
 }
